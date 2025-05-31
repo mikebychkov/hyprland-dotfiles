5456064dcb9b2203901dba3ffc6c6e600cfb0090
mikebychkov <mikebychkov@github.com>
2025-05-31 13:45:45 +0500

    update

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ sudo dnf copr enable solopasha/hyprland -y \
 && sudo dnf update -y \
 && sudo dnf install hyprland hyprland-devel -y \
 && sudo dnf install hyprpaper hyprshot hyprlock hypridle wofi waybar -y
+&& sudo dnf install kitty -y
--- links.sh
+++ links.sh
@@ -4,4 +4,5 @@ ln -s $curr_dir/hypr $HOME/.config/hypr
 ln -s $curr_dir/wallpaper $HOME/.config/wallpaper
 ln -s $curr_dir/waybar $HOME/.config/waybar
 ln -s $curr_dir/wofi $HOME/.config/wofi
+ln -s $curr_dir/kitty $HOME/.config/kitty
 
