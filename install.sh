6d3243ea28739b08116fe0450bfb0ccd763d5079
mikebychkov <mikebychkov@github.com>
2025-05-31 12:47:20 +0500

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo dnf copr enable solopasha/hyprland -y \
+&& sudo dnf update -y \
 && sudo dnf install hyprland hyprland-devel -y \
 && sudo dnf install hyprpaper hyprshot hyprlock hypridle wofi waybar -y
