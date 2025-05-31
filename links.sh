d09359115e7910f5eac9540e0b63d257c5847696
mikebychkov <mikebychkov@github.com>
2025-05-31 13:38:02 +0500

    update

---
+++ links.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curr_dir="$(pwd)"
+ln -s $curr_dir/hypr $HOME/.config/hypr
+ln -s $curr_dir/wallpaper $HOME/.config/wallpaper
+ln -s $curr_dir/waybar $HOME/.config/waybar
+ln -s $curr_dir/wofi $HOME/.config/wofi
+
