2c60188c8008abe0a1cc358558e2a7d20a5a7c55
mikebychkov <mikebychkov@github.com>
2025-05-31 13:17:55 +0500

    update

--- fonts.sh
+++ fonts.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip \
 && wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Hack.zip \
-&& sudo cp -p FiraCode.zip /usr/local/share/fonts/FiraCode/ \
-&& sudo cp -p Hack.zip /usr/local/share/fonts/Hack/ \
+&& sudo mkdir -p /usr/local/share/fonts/FiraCode/ \
+&& sudo mkdir -p /usr/local/share/fonts/Hack/ \
+&& sudo mv FiraCode.zip /usr/local/share/fonts/FiraCode/ \
+&& sudo mv Hack.zip /usr/local/share/fonts/Hack/ \
 && cd /usr/local/share/fonts/FiraCode/ \
 && sudo unzip FiraCode.zip \
 && cd /usr/local/share/fonts/Hack/ \
